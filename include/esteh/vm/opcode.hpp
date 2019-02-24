7e36b8e7979930ce461277e391a7d77c5088d9b9
Ammar Faizi <ammarfaizi2@gmail.com>
2019-02-24 17:09:13 +0700

    Prepare extending opcode

--- include/esteh/vm/opcode.hpp
+++ include/esteh/vm/opcode.hpp
@@ -5,39 +5,40 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct _tehnode_op {
-	
-} tehnode_op;
+typedef union _teavalue_value {
+	long long lval;  // For booleans and integers.
+	double dval;	 // For floating point numbers.
+	struct {		 // For strings
+		char *val;
+		int len;
+	} str;
+} teavalue_value;
+
+typedef struct _teaval {
+	teavalue_value value;
+	uint8_t type;
+} teaval;
 
 typedef struct _esteh_opcode {
-	int code;
-	size_t line;
+	void *handler;		// This is a function pointer that will be invoked to perform the operation of the given opcode.
+	uint16_t code;		// The opcode being executed.
+	uint32_t lineno;	// The line number in the source code the opcode corresponds to.
 	void *content;
 } esteh_opcode;
 
-typedef struct _opcode_sv {
-	int code;
-	uint32_t line;
-	int c_start;
-	size_t c_size;
-} opcode_sv;
-
-typedef struct _opcache_header {
-	size_t opcodes_amount;
-} opcache_header;
 
 #define T_UNKNOWN -1
 
 #define TC_NOTHING 100
 
-#define TD_PRINT 0b000000000
+#define TD_PRINT 200
 
 #define TE_STRING 300
 #define TE_INT 301
 
-#define TE_ADD 1 // +
-#define TE_MIN 2 // -
-#define TE_MUL 4 // *
-#define TE_DIV 8 // /
+#define TF_ADD 1 // +
+#define TF_MIN 2 // -
+#define TF_MUL 4 // *
+#define TF_DIV 8 // /
 
 #endif
