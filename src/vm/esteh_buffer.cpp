de2e66477f6678665ac10d683cf9be25fbc0a8e5
Ammar Faizi <ammarfaizi2@gmail.com>
2019-02-27 15:35:22 +0700

    Update esteh_buffer.cpp

--- src/vm/esteh_buffer.cpp
+++ src/vm/esteh_buffer.cpp
@@ -6,6 +6,7 @@
 
 #define _ESTEH_BUFFER_SIZE 8192
 #define ESTEH_BUFFER_SIZE (_ESTEH_BUFFER_SIZE - 1)
+
 #define FLUSH_BUFFER \
 	write(_fd, __esteh_buffer, allocated_buf); \
 	allocated_buf = 0;
@@ -14,6 +15,8 @@ size_t allocated_buf = 0;
 char *__esteh_buffer = (char *)malloc(ESTEH_BUFFER_SIZE + 2);
 
 void esteh_buffer_add(char *str, size_t len, uint8_t _fd = 1) {	
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wunused-result"
 	if (allocated_buf < ESTEH_BUFFER_SIZE) {
 
 		for (size_t k = 0; (allocated_buf <= ESTEH_BUFFER_SIZE) && (k < len);) {
@@ -30,12 +33,14 @@ void esteh_buffer_add(char *str, size_t len, uint8_t _fd = 1) {
 		if (allocated_buf >= ESTEH_BUFFER_SIZE) {
 			FLUSH_BUFFER
 		}
-	}
-
+	}	
 }
 
 void buffer_shutdown(uint8_t _fd = 1) {
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wunused-result"
 	if (allocated_buf > 0) {
 		FLUSH_BUFFER
 	}
+	#pragma GCC diagnostic pop
 }
