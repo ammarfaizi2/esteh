2b9a380fa3fe688ffa20a668947bdc4fd63aa292
Ammar Faizi <ammarfaizi2@gmail.com>
2019-06-04 15:51:52 +0700

    Prepare intermediate bytecode

--- include/estehvm/vm/token.h
+++ include/estehvm/vm/token.h
@@ -21,32 +21,6 @@ typedef struct _esteh_token {
 	esteh_token_type type;
 } esteh_token;
 
-//
-// Intermediate bytecode
-//
-
-typedef enum _esteh_bytecode_type {
-	tb_print = 1,
-	tb_echo = 2,
-	tb_constant = 3,
-	tb_variable = 4,
-} esteh_bytecode_type;
-
-typedef union _esteh_bytecode_data {
-
-} bytecode_data;
-
-typedef struct _esteh_bytecode {
-	bytecode_data data;
-	esteh_bytecode_type type;
-} esteh_bytecode;
-
-
-
-
-
-
-
 #define ESTEH_TOKEN_SGGT 1024
 uint32_t esteh_vm_lexical_analyze(char *fmap, size_t fsize, esteh_token ***tokens, bool *is_error, char **error_message);
 
